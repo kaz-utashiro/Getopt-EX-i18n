e7d5db780dc21c4d49f69cd15a3100800f53aaa6
Kazumasa Utashiro <kaz@utashiro.com>
2020-06-04 08:55:20 +0900

    minil new Getopt::EX::i18n

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Getopt::EX::i18n - It's new $module
+
+# SYNOPSIS
+
+    use Getopt::EX::i18n;
+
+# DESCRIPTION
+
+Getopt::EX::i18n is ...
+
+# LICENSE
+
+Copyright (C) Kazumasa Utashiro.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Kazumasa Utashiro <kaz@utashiro.com>
---
+++ lib/Getopt/EX/i18n.pm
@@ -0,0 +1,39 @@
+package Getopt::EX::i18n;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Getopt::EX::i18n - It's new $module
+
+=head1 SYNOPSIS
+
+    use Getopt::EX::i18n;
+
+=head1 DESCRIPTION
+
+Getopt::EX::i18n is ...
+
+=head1 LICENSE
+
+Copyright (C) Kazumasa Utashiro.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Kazumasa Utashiro E<lt>kaz@utashiro.comE<gt>
+
+=cut
+
